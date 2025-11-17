67f6d8babd6e5b6849f184e37ed45ca17a54da74
crystalforceix <crystalforceix@gmail.com>
2025-11-17 13:55:26 +0700

    update

--- Water-Hyprland/water-hyprland-global-scripts/exec-scripts/open-fuzzel-screen-capture.sh
+++ Water-Hyprland/water-hyprland-global-scripts/exec-scripts/open-fuzzel-screen-capture.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 setsid sh -c '
-  sleep 0.5 &&
   pkill fuzzel || ~/.config/water-hyprland-global-scripts/fuzzel-scripts/fuzzel-screen-capture.sh
 ' &
 exit 0
--- Water-Hyprland/water-hyprland-global-scripts/exec-scripts/open-fuzzel-swaync-options.sh
+++ Water-Hyprland/water-hyprland-global-scripts/exec-scripts/open-fuzzel-swaync-options.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 setsid sh -c '
-  sleep 0.5 &&
   pkill fuzzel || ~/.config/water-hyprland-global-scripts/fuzzel-scripts/fuzzel-swaync-options.sh
 ' &
 exit 0
