22aa73739eda2adb66ae47a6a8a81d29d23a133b
crystalforceix <crystalforceix@gmail.com>
2026-01-20 12:40:28 +0700

    Hyprland made dots broken

--- Scripts-For-Installer/install-oneui-scripts/one-ui-installer.sh
+++ Scripts-For-Installer/install-oneui-scripts/one-ui-installer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /tmp/ && git clone https://github.com/crystalforceix/OneUI4-Icons && cd OneUI4-Icons/ && mv OneUI-dark ~/.icons && mv OneUI-light ~/.icons && echo "Done! OneUI themes has installed!" && cd /tmp/ && rm -rf OneUI4-Icons/
+cd /tmp/ && git clone https://github.com/end-4/OneUI4-Icons && cd OneUI4-Icons/ && mv OneUI-dark ~/.icons && mv OneUI-light ~/.icons && echo "Done! OneUI themes has installed!" && cd /tmp/ && rm -rf OneUI4-Icons/
